5902580e88969fc7e7fd156031ec3b9bde011475
AhmetEminSaglik <ahmeteminyazilim@gmail.com>
2019-12-05 12:23:33 +0200

    oyunun copyası oluşturuldu ve  yeni tasarımı yenilendi

---
+++ 100 'e ulasma 2. basamak/src/Business/Main.java
@@ -0,0 +1,11 @@
+package Business;
+
+import Gui.BaslangicEkranGui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BaslangicEkranGui beg = new BaslangicEkranGui();
+    }
+
+}
